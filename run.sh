055d593c2e6c7985a1c98c0c2bcdb15250bffed9
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2021-04-29 22:50:45 +0800

    neovim pluginstall using commandline

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ cp init.vim ~/.config/nvim/init.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo 'vim pluginstall!!'
+nvim +PlugInstall +qa
