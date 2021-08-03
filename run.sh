8b9b657fc8f37b188348c048e75fe3177036c8d5
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2021-08-04 00:30:54 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
+# install required packages
 sudo apt remove --purge vim neovim -y;
-sudo apt install cmake -y;
+sudo apt install cmake git python3-pip -y;
+pip3 install --user --upgrade flake8 neovim
 
+# download nvim bin to local/bin
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage ~/.local/bin/nvim
 
-pip3 install --user --upgrade flake8 neovim
-
+# copy nvim config
 cp .vimrc ~/.vimrc
-
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
 
+# install nvim plugin
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 nvim +PlugInstall +qa
