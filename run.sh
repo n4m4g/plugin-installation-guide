b7168b9ec1b1f36016b512d1d77a6019adb0ea08
n4m4g <n4m4g64b4@gmail.com>
2021-11-21 15:46:34 +0800

    update PATH setting

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ pip3 install --user --upgrade flake8 neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage ~/.local/bin/nvim
-echo "export PATH=~/.local/bin/:\$PATH" >> ~/.bashrc
+echo "export PATH=\$PATH:~/.local/bin/" >> ~/.bashrc
 
 # copy nvim config
 cp .vimrc ~/.vimrc
